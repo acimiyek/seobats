dd58f27277be22e6d5503277c3987e30f22b2cba
acimiyek <kevinoin21@gmail.com>
2024-11-09 04:23:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,150 +1,118 @@
-<?php
+<?php require('index-lop.php'); ?
 
-define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'production');
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-switch (ENVIRONMENT)
-{
-	case 'development':
-		error_reporting(-1);
-		ini_set('display_errors', 1);
-	break;
+    <title>Ciputra Artpreneur || Under Construction Page</title>
 
-	case 'testing':
-	case 'production':
-		ini_set('display_errors', 0);
-		if (version_compare(PHP_VERSION, '5.3', '>='))
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-		}
-		else
-		{
-			error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-		}
-	break;
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
-}
+    <!-- Responsive View Meta -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-	$system_path = 'crowsystem';
+    <!-- Place favicon.ico in the root directory -->
+    <link rel="icon" href="favicon.ico" type="image/x-icon">
 
-	$application_folder = 'crowapplication';
+    <!-- Animate.CSS -->
+   
+    <!-- Font Aewsome Style -->
+    <link rel="stylesheet" href="css/font-awesome.min.css">
 
+    <!-- Bootstrap Style -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
 
-	$view_folder = '';
+    <!-- Reset Style -->
+    <link rel="stylesheet" href="css/normalize.css">
 
+    <!-- Main Style -->
+    <link rel="stylesheet" href="css/main.css">
+    
+    <!-- Color Switcher Style -->
+   
+    
+    <!--[if lt IE 9]>
+	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
 
+  
+</head>
+<body>
+<!--[if lt IE 8]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+<![endif]-->
+<!-- screen loader start -->
 
-	if (defined('STDIN'))
-	{
-		chdir(dirname(__FILE__));
-	}
-
-	if (($_temp = realpath($system_path)) !== FALSE)
-	{
-		$system_path = $_temp.DIRECTORY_SEPARATOR;
-	}
-	else
-	{
-		// Ensure there's a trailing slash
-		$system_path = strtr(
-			rtrim($system_path, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		).DIRECTORY_SEPARATOR;
-	}
-
-	// Is the system path correct?
-	if ( ! is_dir($system_path))
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your system folder path does not appear to be set correctly. Please open the following file and correct this: '.pathinfo(__FILE__, PATHINFO_BASENAME);
-		exit(3); // EXIT_CONFIG
-	}
+<!-- preload start -->
 
 
-	// The name of THIS file
-	define('SELF', pathinfo(__FILE__, PATHINFO_BASENAME));
+<!-- === Header Section === -->
+<div id="container">
+	<!-- +++ Intro Section ++++ -->
+    <section class="intro">
+    	<!-- Intro Section Background-image -->
+    	<div class="intro-image"></div>
+		
+		<!-- ====+++ Intro Section Content +++==== -->
+		<div class="center-content">
+			
 
-	// Path to the system directory
-	define('BASEPATH', $system_path);
+			<div class="intro-content text-center">	
+				<!-- Logo -->								
+				<div class="logo">
+					<h2 class="text-logo">Ciputra Artpreneur</h2>
+				</div><!-- /End logo -->							
+				<!-- ==== Main Heading ==== -->												
+				<h1 class="intro-title">We are Coming Soon<span class="obak">!</span></h1>
 
-	// Path to the front controller (this file) directory
-	define('FCPATH', dirname(__FILE__).DIRECTORY_SEPARATOR);
+				<!-- Spin Clock -->
+			
+						
+				<!-- ====+++ Countdown Timer +++==== -->						
+								
+				
+				<!-- ==== Intro Section Sub-Heading ==== -->				
+				<div class="intro-subtitle">
+					<p>We are currently Creating Something Awesome.</p>
+				</div>							
+											
+			</div> <!-- /End Intro Section Content -->
+		</div>
+		<!-- ==== Intro Section Bottom Content ==== -->
+			
+	</section><!-- /end intro section -->
 
-	// Name of the "system" directory
-	define('SYSDIR', basename(BASEPATH));
+	<!-- +++ Trigger After Content +++ -->
+	
 
-	// The path to the "application" directory
-	if (is_dir($application_folder))
-	{
-		if (($_temp = realpath($application_folder)) !== FALSE)
-		{
-			$application_folder = $_temp;
-		}
-		else
-		{
-			$application_folder = strtr(
-				rtrim($application_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(BASEPATH.$application_folder.DIRECTORY_SEPARATOR))
-	{
-		$application_folder = BASEPATH.strtr(
-			trim($application_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your application folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+<!-- Style Switcher -->
 
-	define('APPPATH', $application_folder.DIRECTORY_SEPARATOR);
+<!-- jQuery Library -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.2.min.js"><\/script>')</script>
+<!-- Modernizer -->
 
-	// The path to the "views" directory
-	if ( ! isset($view_folder[0]) && is_dir(APPPATH.'views'.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.'views';
-	}
-	elseif (is_dir($view_folder))
-	{
-		if (($_temp = realpath($view_folder)) !== FALSE)
-		{
-			$view_folder = $_temp;
-		}
-		else
-		{
-			$view_folder = strtr(
-				rtrim($view_folder, '/\\'),
-				'/\\',
-				DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-			);
-		}
-	}
-	elseif (is_dir(APPPATH.$view_folder.DIRECTORY_SEPARATOR))
-	{
-		$view_folder = APPPATH.strtr(
-			trim($view_folder, '/\\'),
-			'/\\',
-			DIRECTORY_SEPARATOR.DIRECTORY_SEPARATOR
-		);
-	}
-	else
-	{
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'Your view folder path does not appear to be set correctly. Please open the following file and correct this: '.SELF;
-		exit(3); // EXIT_CONFIG
-	}
+<!-- Bootstrap JS -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Classie JS -->
+<script src="js/classie.js"></script>       
+<!-- Countdown JS -->
+<!-- Goole map API -->
+<!-- Nice Scroll JS -->
+<script src="js/jquery.nicescroll.3.5.4.js"></script>
+<!-- Style Switcher JS -->
+<script src="switcher/switcher.js"></script>
+<!-- Plugin Initialization js -->
+<script src="js/plugins.js"></script>
+<!-- Custom JS -->
+<script src="js/main.js"></script>
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
 
-	define('VIEWPATH', $view_folder.DIRECTORY_SEPARATOR);
-
-require_once BASEPATH.'core/CodeIgniter.php';
+</body>
+</html>
